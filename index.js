9463af611e2762f411e06c8d942e73115b2a3745
Soumik Dalei <soumikdalei@gmail.com>
2024-02-19 22:34:33 +0530

    Added some Features

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const p2button=document.querySelector("#p2")
 const p1display=document.querySelector("#player1")
 const p2display=document.querySelector("#player2")
 const resetbutton=document.querySelector("#r")
-const winninigscorevalue=document.querySelector("#e")
+const winningscorevalue=document.querySelector("#playto")
 let count1=0;
 let count2=0;
 let winningscore=5;
@@ -12,6 +12,8 @@ p1button.addEventListener('click',function(){
     if(!gameover){count1++;
     if(count1===winningscore){
         gameover=true
+        p1display.classList.add('winner')
+        p2display.classList.add('loser')
     }
     p1display.textContent=count1;}
 })
@@ -19,15 +21,21 @@ p2button.addEventListener('click',function(){
     if(!gameover){count2++;
     if(count2===winningscore){
         gameover=true
+        p2display.classList.add('winner')
+        p1display.classList.add('loser')
     }
     p2display.textContent=count2;}
 })
-winningscore.addEventListener('change',function(){
+winningscorevalue.addEventListener('change',function(){
     winningscore=parseInt(this.value)
+    reset();
 })
-resetbutton.addEventListener('click',function(){
+resetbutton.addEventListener('click',reset)
+function reset(){
     gameover=false
     count1=0; count2=0;
     p1display.textContent=count1
     p2display.textContent=count2
-})
+    p1display.classList.remove('winner','loser')
+    p2display.classList.remove('winner','loser')
+}
